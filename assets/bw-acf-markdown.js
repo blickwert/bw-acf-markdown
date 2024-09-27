95a3477fde1fc6eee32bf6d65d5c2a5e39317428
David Wögerer <info@blickwert.at>
2024-09-27 15:17:18 +0200

    Update bw-acf-markdown.js

--- assets/bw-acf-markdown.js
+++ assets/bw-acf-markdown.js
@@ -13,14 +13,25 @@ jQuery(document).ready(function($) {
             .replace(/\*(.*?)\*/gim, '<em>$1</em>') // Kursiv
             .replace(/\n/g, '<br>'); // Zeilenumbruch in <br>
 
-        // Setze den formatierten Text direkt ins Textarea-Feld
+        // Setze den formatierten Text direkt ins Textarea-Feld (Vorschau im Editor simulieren)
         $(this).html(formatted);
     });
 
     // Für WYSIWYG-Felder (TinyMCE)
     tinymce.init({
-        selector: '.acf-wysiwyg textarea',
+        selector: '.acf-wysiwyg textarea', // Auswahl des WYSIWYG-Felds
+        menubar: false, // Entferne das Menü, falls nicht benötigt
+        toolbar: 'undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image', // Toolbar anpassen
+        plugins: 'lists link image', // Nutze nur die Plugins, die wirklich benötigt werden
         setup: function(editor) {
+            // Verhindere die Standardumwandlung von Markdown-Syntax in HTML durch TinyMCE
+            editor.on('keydown', function(e) {
+                if (e.key === '#' || e.key === '-' || e.key === '*') {
+                    e.preventDefault(); // Stoppe die automatische Konvertierung von TinyMCE
+                }
+            });
+
+            // Eigene Logik zur Umwandlung von Markdown in HTML bei Eingabe im Editor
             editor.on('keyup input', function(e) {
                 var input = editor.getContent({ format: 'text' });
 
